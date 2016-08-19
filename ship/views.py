c29e5d29d7f2a43352958e3b4e9e66f3dba6bfbc
ald.dev <mis@alder.com.tw>
2016-08-19 18:25:15 +0800

    Stage reportlab

--- ship/views.py
+++ ship/views.py
@@ -4,30 +4,82 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
 from django.utils import timezone
 
-from reportlab.pdfgen import canvas
 from .models import QuoteHead, Customer
 from .forms import QuoteHeadCreateForm, QuoteLineCreateForm
 
 # Create your views here.
-
+from reportlab.pdfgen import canvas
+from reportlab.lib.pagesizes import letter, A4
+from reportlab.lib.colors import pink, green, brown, white, black, blue
 def export_pdf(request):
+
     # Create the HttpResponse object with the appropriate PDF headers.
+
+
     response = HttpResponse(content_type='application/pdf')
     response['Content-Disposition'] = 'attachment; filename="Quotetion.pdf"'
 
     # Create the PDF object, using the response object as its "file."
+    record = get_object_or_404( QuoteHead, id=1)
+    print( dir( record ))
+    '''
+    request_user = models.CharField(max_length=60, null=False, blank=False) #開單人
+    order_number = models.IntegerField(null=True, blank=True, unique=True) #報價單號
+    ord_date = models.DateField(default=timezone.now) #報價日期
+    customer = models.ForeignKey(Customer) #客戶編號
+    effective_date = models.DateField( default=timezone.now ) # 報價單有效日期
+    currency = models.ForeignKey( Currency )
+    invalid = models.BooleanField(default=False) #作廢
+    memo = models.TextField(null=True, blank=True)
+    '''
+
     p = canvas.Canvas(response)
+    date, *_ = str(timezone.now()).split(' ')
+
 
     # Draw things on the PDF. Here's where the PDF generation happens.
     # See the ReportLab documentation for the full list of functionality.
     #seal = '/static/img/alder_logo.png'
     #p.drawImage(seal, 210,800, width=None, height=None)
-    p.setFont('Helvetica', 24)
-    p.drawCentredString(210, 800, "Alder Optomechanical Corp.")
 
-    p.setFont('Helvetica', 18)
-    p.drawString(190, 780, "QUOTATION")
+    p.setFont('Helvetica', 20)
+    p.drawCentredString(310, 810, "Alder Optomechanical Corp.")
+    p.setFont('Helvetica', 16)
+    p.drawCentredString(310, 795, "Quotetion")
+    p.setFont('Helvetica', 10)
+    p.drawRightString( 580, 810 , "Print Date: " + date)
 
+
+    
+    p.drawRightString( 150, 660 , "Sales Name:"  )
+    p.drawRightString( 150, 650 , "Order Number:" )
+    p.drawRightString( 150, 640 , "Order Date:"  )
+    p.drawRightString( 150, 630 , "Effective Date:" )
+    p.drawRightString( 150, 620 , "Currency:"  )
+    p.drawRightString( 150, 610 , "Comment:"  )
+
+    p.drawString( 160, 660 , record.request_user )
+    p.drawString( 160, 650 ,  str(record.order_number))
+    p.drawString( 160, 640 ,  str(record.ord_date) )
+    p.drawString( 160, 630 ,  str(record.effective_date))
+    p.drawString( 160, 620 ,  str(record.currency) )
+    p.drawString( 160, 610 ,  str(record.memo) )
+
+
+
+    '''
+    p.setFont('Helvetica', 10)
+    p.drawRightString( 580, 610 , date)
+    p.setFillColor(blue)
+
+    p.setTitle("QUOTATION")
+    y = 780
+    for font in p.getAvailableFonts():
+        y-=10
+        p.drawRightString(150, y, font+":") # 向右對齊
+        p.drawCentredString(150, y, font+":") # 置中對齊
+    p.drawString(190, 780, "Hello World")
+    '''
     # Close the PDF object cleanly, and we're done.
     p.showPage()
     p.save()
